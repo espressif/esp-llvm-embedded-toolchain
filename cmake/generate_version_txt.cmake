d2a89b8a4e7f653a299362711e7e74a91c280988
Alexey Gerenkov <alexey@espressif.com>
2025-02-25 23:02:16 +0300

    Update components versions description file

--- cmake/generate_version_txt.cmake
+++ cmake/generate_version_txt.cmake
@@ -39,6 +39,22 @@ if(NOT "${newlib_SOURCE_DIR}" STREQUAL "")
         COMMAND_ERROR_IS_FATAL ANY
     )
 endif()
+if(NOT "${binutils_SOURCE_DIR}" STREQUAL "")
+    execute_process(
+        COMMAND git -C ${binutils_SOURCE_DIR} rev-parse HEAD
+        OUTPUT_VARIABLE binutils_COMMIT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endif()
+if(NOT "${xtensa_overlays_SOURCE_DIR}" STREQUAL "")
+    execute_process(
+        COMMAND git -C ${xtensa_overlays_SOURCE_DIR} rev-parse HEAD
+        OUTPUT_VARIABLE xtensa_overlays_COMMIT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endif()
 
 configure_file(
     ${CMAKE_CURRENT_LIST_DIR}/VERSION.txt.in
